18b53caf1e8911f43d0f74ff69f496ff096b106c
iko4akov <119832167+iko4akov@users.noreply.github.com>
2023-01-19 09:02:58 +0300

    update view.py

--- bp_find_raiting/view.py
+++ bp_find_raiting/view.py
@@ -1,7 +1,7 @@
 from bp_find_raiting.dao.utils import find_rating
 from flask import Blueprint
 
-
+#Создание блюпринтов
 children_blueprint = Blueprint('children_blueprint', __name__)
 family_blueprint = Blueprint('family_blueprint', __name__)
 adult_blueprint = Blueprint('adult_blueprint', __name__)
@@ -18,8 +18,8 @@ def page_find_family():
     result = find_rating(["G", "PG", "PG-13"])
     return result
 
+
 @adult_blueprint.get('/adult')
 def page_find_adult():
     result = find_rating(["R", "NC-17"])
     return result
-
